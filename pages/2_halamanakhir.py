fedc3ebb68c4019ac5e31ba0c1a6bb14f5d2f585
lilyaa19-art <lailialfxai119@gmail.com>
2025-05-28 14:27:20 +0700

    Update 2_halamanakhir.py

--- pages/2_halamanakhir.py
+++ pages/2_halamanakhir.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.set_page_config(page_tittle="Halaman Akhir", page_icon"📈")
-st.tittle("Tes")
+st.set_page_config(page_title="Halaman Akhir", page_icon"📈")
+st.title("Tes")
 st.sidebar.header("Halaman Ahir")
